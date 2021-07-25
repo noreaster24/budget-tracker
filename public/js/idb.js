112b27a55e93f0e6e0a0925ae03fc2c5a9ff9911
Joe Porrazzo <joepa2624@me.com>
2021-07-24 19:54:56 -0700

    fixed mongo issue

---
+++ public/js/idb.js
@@ -0,0 +1,62 @@
+let db;
+// establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+};
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    // open a new transaction with the database  
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    transactionObjectStore.add(record);
+};
+
+function uploadTransaction() {
+
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        // send data to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+                    const transactionObjectStore = transaction.objectStore('new_transaction');
+                    transactionObjectStore.clear();
+
+                    alert('All saved transactions have been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+};
